82d99420bd3921e1e7e44c363ef6c2b5bb133998
soljinn <soljin97@gmail.com>
2023-06-06 15:10:35 +0700

    update

--- case-study/src/App.js
+++ case-study/src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
 
 function App() {
-  return (
-      <>
-      <Header/>
-      <Footer/>
-      </>
-  );
+    return (
+        <>
+            <Header/>
+            <h1>ffff</h1>
+            <h1>ffff</h1>
+            <h1>ffff</h1>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
